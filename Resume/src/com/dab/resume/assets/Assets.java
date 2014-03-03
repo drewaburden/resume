ce62ffe769aa39f5ba526750f10e449365603d32
drewaburden <Drewaburden@gmail.com>
2014-03-02 22:31:02 -0600

    Added some helper methods for fetching and creating animations to hopefully help clean up some of the massive blocks of animation loading code.

--- Resume/src/com/dab/resume/assets/Assets.java
+++ Resume/src/com/dab/resume/assets/Assets.java
@@ -14,6 +14,9 @@
 package com.dab.resume.assets;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public final class Assets extends AssetManager {
 	private static Assets instance;
@@ -24,10 +27,24 @@ public final class Assets extends AssetManager {
 		if (instance == null) instance = new Assets();
 		return instance;
 	}
-
 	@Override
 	public synchronized void dispose() {
 		super.dispose();
 		instance = null;
+	}
+
+	public Animation getAnimation(String filename, int numFrames, float frameDuration) {
+		Texture texture = Assets.getInstance().get(filename);
+		texture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+		TextureRegion[][] tmp = TextureRegion.split(texture, texture.getWidth() / numFrames, texture.getHeight());
+		TextureRegion[] frames = new TextureRegion[numFrames];
+		int index = 0;
+		for (TextureRegion[] rows : tmp) {
+			for (TextureRegion cols : rows) {
+				frames[index] = cols;
+				index++;
+			}
+		}
+		return new Animation(frameDuration, frames);
 	}
 }
